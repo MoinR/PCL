161db1c51fd0415ca61465d79fe54d10ae89c00f
Moin R <31372103+MoinR@users.noreply.github.com>
2019-10-03 20:31:28 +0530

    Update q3.c

--- unit-4/q3.c
+++ unit-4/q3.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 void main(){
-    int num,rem,i,j;
+    int num,rem,i;
     int bin[10];
 
     printf("Enter number :");
     scanf("%d",&num);
 
-    i = 0; j = 0;
+    i = 0;
     while(num > 0){
         rem = num % 2;
         num = num / 2;
         bin[i] = rem;
         i++;
-        j++;
     }
-    for(int k = j-1; k>= 0; k--){
+    for(int k = i-1; k>= 0; k--){
         printf("%d",bin[k]);
     }
 }
